e7ff7abc45ba9b3e6d85017d478aaa13134a94fb
upcwangying <upcwangying@126.com>
2020-10-09 09:36:48 +0800

    feat: spreadsheet

--- src/components/SpreadSheet.vue
+++ src/components/SpreadSheet.vue
@@ -10,7 +10,22 @@ export default {
   name: 'SpreadSheet',
   mounted() {
     // const s = new Spreadsheet('#xapp')
-    const s = new Spreadsheet(this.$refs.xspreadsheet)
+    const s = new Spreadsheet(this.$refs.xspreadsheet, {
+      view: {
+        height: () => document.documentElement.clientHeight,
+        width: () => document.documentElement.clientWidth
+      },
+      row: {
+        height: 25,
+        len: 30
+      },
+      col: {
+        len: 11,
+        width: 100,
+        indexWidth: 60,
+        minWidth: 60,
+      }
+    })
       .loadData({}) // load data
       .change(data => {
         // save data to db
